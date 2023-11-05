19eca897cf5adf39c5feef09d93911ca336bbff5
ryowuandjanet <ryowu0329@gmail.com>
2023-11-05 08:43:52 +0800

    75 尚矽谷 Vue3 todoList案例拆分組件

--- src/App.vue
+++ src/App.vue
@@ -1,38 +1,39 @@
 <template>
   <div class="todo-container">
     <div class="todo-wrap">
-      <div class="todo-header">
-        <input type="text" placeholder="请输入你的任务名称，按回车键确认"/>
-      </div>
-      <ul class="todo-main">
-        <li>
-          <label>
-            <input type="checkbox"/>
-            <span>xxxxx</span>
-          </label>
-          <button class="btn btn-danger" style="display:none">删除</button>
-        </li>
-        <li>
-          <label>
-            <input type="checkbox"/>
-            <span>yyyy</span>
-          </label>
-          <button class="btn btn-danger" style="display:none">删除</button>
-        </li>
-      </ul>
-      <div class="todo-footer">
-        <label>
-          <input type="checkbox"/>
-        </label>
-        <span>
-          <span>已完成0</span> / 全部2
-        </span>
-        <button class="btn btn-danger">清除已完成任务</button>
-      </div>
+      <Header />
+      <List />
+      <Footer />
     </div>
   </div>
 </template>
 
+<script lang="ts">
+  import { defineComponent } from 'vue'
+  import Header from './components/Header.vue'
+  import List from './components/List.vue'
+  import Footer from './components/Footer.vue'
+  export default defineComponent ({
+    name: 'App',
+    components: {
+      Header,
+      List,
+      Footer
+    }
 
-<script>
+  })
 </script>
+
+<style>
+  /*app*/
+  .todo-container {
+    width: 600px;
+    margin: 0 auto;
+  }
+  .todo-container .todo-wrap {
+    padding: 10px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+  }
+</style>
+
